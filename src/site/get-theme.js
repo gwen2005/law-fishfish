1ca488d0e9c88aeeec31a6389c4d95996d24369b
gwen2005 <165680831+gwen2005@users.noreply.github.com>
2024-10-13 21:10:15 +0800

    Update get-theme.js

--- src/site/get-theme.js
+++ src/site/get-theme.js
@@ -1,11 +1,13 @@
-<script type="text/javascript">
-  var password = prompt("请输入密码:");
-  if (password !== "886586") {
+// 使用 prompt 要求用户输入密码
+const password = prompt("请输入密码:");
+
+// 检查密码是否正确
+if (password !== "886586") {
     document.body.innerHTML = "Access Denied.";
-  } else {
+} else {
+    // 如果密码正确，则继续加载应用
     document.body.style.display = "block";
-  }
-</script>
+}
 
 require("dotenv").config();
 const axios = require("axios");
