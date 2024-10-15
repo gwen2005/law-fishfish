44ad91cdbfb6b586456616b40f280e60e4a8fa95
gwen2005 <165680831+gwen2005@users.noreply.github.com>
2024-10-15 23:12:40 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,20 +1,15 @@
-import { NextResponse } from 'next/server';
+const { NextResponse } = require('next/server');
 
-export function middleware(req) {
+function middleware(req) {
   const url = req.nextUrl;
 
-  // 示例：获取请求中的自定义 Header
-  const password = req.headers.get('x-password');
+  const password = req.headers['x-password'];
 
-  // 检查密码是否匹配
   if (password === '886586') {
-    return NextResponse.next(); // 继续处理请求
+    return NextResponse.next();
   }
 
-  // 如果密码不匹配，返回 401 Unauthorized 响应
   return new Response('Invalid Password', { status: 401 });
 }
 
-export const config = {
-  matcher: ['/protected/*'], // 你可以指定匹配路径
-};
+module.exports = { middleware };
