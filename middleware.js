30b297fa1028201e2c83223fe76601a32509763e
gwen2005 <165680831+gwen2005@users.noreply.github.com>
2024-10-15 23:05:35 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,27 +1,20 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  // 设定用户名和密码
-  const username = 'chenfeiyan';
-  const password = '886586';
+  const url = req.nextUrl;
 
-  // 生成 `Basic` 认证的授权头
-  const basicAuth = req.headers.get('authorization');
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1];
-    const [user, pass] = atob(authValue).split(':');
+  // 示例：获取请求中的自定义 Header
+  const password = req.headers.get('x-password');
 
-    // 检查用户提供的用户名和密码是否匹配
-    if (user === username && pass === password) {
-      return NextResponse.next();
-    }
+  // 检查密码是否匹配
+  if (password === '886586') {
+    return NextResponse.next(); // 继续处理请求
   }
 
-  // 如果未通过认证，返回 401 Unauthorized 响应，并请求输入用户名和密码
-  return new Response('Authentication required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  });
+  // 如果密码不匹配，返回 401 Unauthorized 响应
+  return new Response('Invalid Password', { status: 401 });
 }
+
+export const config = {
+  matcher: ['/protected/*'], // 你可以指定匹配路径
+};
